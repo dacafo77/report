e54e8349a279e98ee3cb9757f4313444d3e71248
choi <dacafo77@nate.com>
2024-07-01 10:27:46 +0900

    11

--- app/src/main/java/com/example/mylogin/HomeActivity.kt
+++ app/src/main/java/com/example/mylogin/HomeActivity.kt
@@ -3,11 +3,13 @@ package com.example.mylogin
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +21,17 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val randomimg = findViewById<ImageView>(R.id.logo)
+        when(Random.nextInt(1, 4)){
+            1 -> randomimg.setImageResource(R.drawable.logo)
+            2 -> randomimg.setImageResource(R.drawable.welcome)
+            3 -> randomimg.setImageResource(R.drawable.logo2)
+        }
+
+
+
+
         val signinId = intent.getStringExtra("아이디:")
 
         val textView = findViewById<TextView>(R.id.tv_id)
@@ -29,6 +42,8 @@ class HomeActivity : AppCompatActivity() {
         btnClose.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
+            finish()
+
         }
     }
 }
